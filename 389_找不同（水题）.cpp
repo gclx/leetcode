964c2495effa34a102f7f4c6d8a1bb43911813cc
gclx <17705852351@163.com>
2020-12-18 23:25:09 +0800

    2020_12_18

---
+++ 389_找不同（水题）.cpp
@@ -0,0 +1,21 @@
+/**
+	
+	给定两个字符串 s 和 t，它们只包含小写字母。
+
+	字符串 t 由字符串 s 随机重排，然后在随机位置添加一个字母。
+
+	请找出在 t 中被添加的字母
+
+**/
+
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        for (int i = 0; i < s.size(); i++)
+            if (s[i] != t[i])
+                return t[i];
+        return t[t.size()-1];
+    }
+};
